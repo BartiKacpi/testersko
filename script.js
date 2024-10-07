83c6e74a0468b849f2749896b6265dd121c2d5c3
BartiKacpi <theprojectlsw@gmail.com>
2024-10-07 12:28:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,11 @@
-function searchArticles() {
-    let input = document.getElementById('searchInput').value.toLowerCase();
-    let articles = document.getElementsByClassName('article');
+function displayArticle(articleId) {
+    let articles = document.getElementsByClassName('article-content');
 
     for (let i = 0; i < articles.length; i++) {
-        let article = articles[i];
-        if (article.innerText.toLowerCase().includes(input)) {
-            article.style.display = "";
-        } else {
-            article.style.display = "none";
-        }
+        articles[i].style.display = 'none';
     }
-}
 
-function filterArticles(category) {
-    let articles = document.getElementsByClassName('article');
-
-    for (let i = 0; i < articles.length; i++) {
-        let article = articles[i];
-        if (article.getAttribute('data-category') === category || category === 'all') {
-            article.style.display = "";
-        } else {
-            article.style.display = "none";
-        }
-    }
-}
-
-function loadMoreArticles() {
-    let articleTitles = [
-        "Nowe odkrycia w badaniach nad lwami",
-        "Tygrysy na wolności: wyzwania i ochrona",
-        "Recenzja: film 'Królestwo Kotów'"
-    ];
-    let articleDescriptions = [
-        "Lwy mają unikalne zachowania społeczne. Badania pokazują, że...",
-        "Tygrysy są zagrożone, ale istnieją liczne inicjatywy...",
-        "Film 'Królestwo Kotów' przedstawia niesamowite sceny życia dzikich..."
-    ];
-    let randomIndex = Math.floor(Math.random() * articleTitles.length);
-
-    let newArticle = document.createElement('div');
-    newArticle.classList.add('article');
-    newArticle.innerHTML = `
-        <h3><a href="#">${articleTitles[randomIndex]}</a></h3>
-        <p>${articleDescriptions[randomIndex]}</p>
-    `;
-    document.getElementById('articles').insertBefore(newArticle, document.getElementById('loadMore'));
+    document.getElementById(articleId).style.display = 'block';
 }
 
 function updateVisitCount() {
